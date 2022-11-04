870744e6a3f4c3681440fa1cb9eb2530ff134b3e
alexabruck <5918060+alexabruck@users.noreply.github.com>
2022-11-04 18:38:02 +0100

    Fix landing page layout

--- src/screens/0_Start.js
+++ src/screens/0_Start.js
@@ -3,12 +3,12 @@ import VerticalGrid from "../components/VerticalGrid";
 
 function Screen() {
   return (
-    <div className="bg-gray -m-6 h-screen">
-      <VerticalGrid>
+    <div className="bg-gray -m-6 flex-grow flex flex-col justify-center">
+      <VerticalGrid className="w-full px-4">
         <ButtonNext to="/registrierung/start">Registrierung</ButtonNext>
         <ButtonNext to="/login/anmeldung">Login</ButtonNext>
         <ButtonNext to="/zugast/start">Ich bin zu Gast</ButtonNext>
-        <div className="grid grid-cols-3 gap-6">
+        <div className="grid grid-cols-3 gap-6 fixed w-full left-0 bottom-0">
           <div className="Logo Logo--palm flex-grow"></div>
           <div className="Logo Logo--drosos flex-grow"></div>
           <div className="Logo Logo--beisheim flex-grow"></div>
