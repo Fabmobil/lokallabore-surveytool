61f147df9f2e9cae3764ed14998240186dd1f365
Alexa Steinbrück <5918060+alexabruck@users.noreply.github.com>
2023-01-06 17:36:54 +0100

    Fix Robi position

--- src/screens/Gast/08_Vorhaben.js
+++ src/screens/Gast/08_Vorhaben.js
@@ -1,22 +1,60 @@
+import { useRef, useState, useEffect } from "react";
 import WeiterButton from "../../components/WeiterButton";
 import TextInput from "../../components/TextInput";
 import RobiGif from "../../components/RobiGif";
 import RobiSliderGif from "../../assets/robi-gifs/Robi_slider-min.gif";
+
+const robiWidth = 263;
 
 function Screen({ onSubmit, data, nextRoute }) {
   function hasUserAnswered() {
     return !!data;
   }
 
+  const containerRef = useRef(null);
+  const [robiY, setRobiY] = useState(0);
+  const [robiX, setRobiX] = useState(0);
+
+  function updateRobiX() {
+    const textBox = containerRef.current.firstElementChild;
+    const target = textBox.getBoundingClientRect().x + textBox.offsetWidth / 2;
+    setRobiX(target - robiWidth + 80);
+  }
+
+  function updateRobiY() {
+    const textBox = containerRef.current.firstElementChild;
+    setRobiY(textBox.offsetTop + textBox.offsetHeight);
+  }
+
+  useEffect(() => {
+    updateRobiX();
+    updateRobiY();
+
+    const resizeListener = () => {
+      updateRobiX();
+      updateRobiY();
+    };
+    window.addEventListener("resize", resizeListener);
+    return () => {
+      window.removeEventListener("resize", resizeListener);
+    };
+  }, [data]);
+
   return (
     <>
       <p>Was möchtest du hier gerne machen?</p>
-      <div className="vertical-center">
+      <div className="vertical-center" ref={containerRef}>
         <TextInput value={data} onChange={(val) => onSubmit(val)} />
       </div>
       <RobiGif
+        className="fixed"
         src={RobiSliderGif}
-        style={{ width: 263, transform: "rotate(159.46deg)" }}
+        style={{
+          width: robiWidth,
+          transform: "rotate(159.46deg)",
+          left: robiX + 42,
+          top: robiY - 30,
+        }}
       />
       <WeiterButton disabled={!hasUserAnswered()} navigateTo={nextRoute} />
     </>
