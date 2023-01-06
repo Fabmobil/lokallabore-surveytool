ebbd4b259063de43ad23bc6558a93f263be77fee
Alexa Steinbrück <5918060+alexabruck@users.noreply.github.com>
2023-01-06 15:24:44 +0100

    Persist date as ISO 🙈

--- src/App.js
+++ src/App.js
@@ -24,9 +24,9 @@ class App extends React.Component {
 
   getValueOrNone(surveyState, val1, val2) {
     try {
-      return val2 ? surveyState[val1][val2] : surveyState[val1]
+      return val2 ? surveyState[val1][val2] : surveyState[val1];
     } catch {
-      return undefined
+      return undefined;
     }
   }
 
@@ -86,7 +86,7 @@ class App extends React.Component {
   submitAllAnswersRegistrierung() {
     const data = {
       ...this.state.surveyAnswersRegistrierung,
-      date: new Date().toLocaleString(),
+      date: new Date().toISOString(),
     };
     console.log("submitted data", data);
     this.firebaseClient
@@ -97,7 +97,7 @@ class App extends React.Component {
   submitAllAnswersLogin() {
     const data = {
       ...this.state.surveyAnswersLogin,
-      date: new Date().toLocaleString(),
+      date: new Date().toISOString(),
     };
     console.log("submitted data", data);
     return this.firebaseClient
@@ -106,11 +106,11 @@ class App extends React.Component {
   }
 
   reportError(errorCode) {
-    this.setState({ ...this.state, error: errorCode })
+    this.setState({ ...this.state, error: errorCode });
   }
 
   clearError() {
-    this.setState({ ...this.state, error: null })
+    this.setState({ ...this.state, error: null });
   }
 
   createNewUser() {
@@ -119,15 +119,17 @@ class App extends React.Component {
 
     return this.firebaseClient.userDoesExist(userID).then((doesExist) => {
       if (doesExist) {
-        this.reportError('USER_EXISTS');
+        this.reportError("USER_EXISTS");
         throw Error();
       } else {
         this.firebaseClient
           .postUser(userID)
-          .then(() => { this.logAnswerRegistrierung('userID', userID) })
+          .then(() => {
+            this.logAnswerRegistrierung("userID", userID);
+          })
           .catch(() => {
             if (window.confirm("Oh no! Ein Fehler ist aufgetreten.")) {
-              window.location = "/"
+              window.location = "/";
             }
           });
       }
@@ -137,7 +139,7 @@ class App extends React.Component {
   submitAllAnswersGuest() {
     const data = {
       ...this.state.surveyAnswersGuest,
-      date: new Date().toLocaleString(),
+      date: new Date().toISOString(),
     };
     console.log("submitted data", data);
     this.firebaseClient
@@ -156,8 +158,15 @@ class App extends React.Component {
 
   onFinalSubmitLogin() {
     return this.submitAllAnswersLogin()
-      .then(() => { this.resetSurveyData(); console.log("reset!") })
-      .then(() => { this.firebaseClient.incrementNumberOfVisits(this.state.surveyAnswersLogin.userID) });
+      .then(() => {
+        this.resetSurveyData();
+        console.log("reset!");
+      })
+      .then(() => {
+        this.firebaseClient.incrementNumberOfVisits(
+          this.state.surveyAnswersLogin.userID
+        );
+      });
   }
 
   getNextRoute(schema, i) {
@@ -168,128 +177,144 @@ class App extends React.Component {
   }
 
   saveNumberOfVisits(userId) {
-    this.firebaseClient.getNumberOfVisits(userId)
-      .then((number) => { this.logAnswerLogin("calculatedNumberOfVisits", number) })
+    this.firebaseClient.getNumberOfVisits(userId).then((number) => {
+      this.logAnswerLogin("calculatedNumberOfVisits", number);
+    });
   }
 
   render() {
+    return (
+      <>
+        <AlertLayer
+          error={this.state.error}
+          onCloseClick={() => this.clearError()}
+          onAbandonConfirmClick={() => {
+            this.resetSurveyData();
+            window.location = "/";
+          }}
+        />
+        <BrowserRouter>
+          <Routes>
+            <Route path="" element={<Layout2 />}>
+              <Route path="" element={<StartScreen />}></Route>
+            </Route>
+            <Route
+              path="/"
+              element={
+                <Layout
+                  onLogoClick={(e) => {
+                    e.preventDefault();
+                    this.reportError("ABANDON");
+                  }}
+                />
+              }
+            >
+              {SURVEY_REGISTRIERUNG.surveyItems.map((item, i) => (
+                <Route
+                  key={item.questionId}
+                  path={`${SURVEY_REGISTRIERUNG.baseUrl}/${item.questionId}`}
+                  element={
+                    <item.screenComponent
+                      nextRoute={this.getNextRoute(SURVEY_REGISTRIERUNG, i)}
+                      onSubmit={(data) => {
+                        this.logAnswerRegistrierung(item.questionId, data);
+                      }}
+                      data={
+                        this.state.surveyAnswersRegistrierung[item.questionId]
+                      }
+                      globalData={{
+                        nickname:
+                          this.state.surveyAnswersRegistrierung["nickname"],
+                      }}
+                      onNicknameSubmit={() => this.createNewUser()}
+                      onFinalSubmit={() => this.onFinalSubmitRegistrierung()}
+                      onLogout={() => this.resetSurveyData()}
+                    ></item.screenComponent>
+                  }
+                />
+              ))}
 
-    return (<>
-      <AlertLayer error={this.state.error} onCloseClick={() => this.clearError()} onAbandonConfirmClick={() => { this.resetSurveyData(); window.location = "/" }} />
-      <BrowserRouter>
-        <Routes>
-          <Route path="" element={<Layout2 />}>
-            <Route path="" element={<StartScreen />}></Route>
-          </Route>
-          <Route
-            path="/"
-            element={
-              <Layout
-                onLogoClick={(e) => {
-                  e.preventDefault();
-                  this.reportError('ABANDON')
-                }}
-              />
-            }
-          >
+              {SURVEY_LOGIN.surveyItems.map((item, i) => (
+                <Route
+                  key={item.questionId}
+                  path={`${SURVEY_LOGIN.baseUrl}/${item.questionId}`}
+                  element={
+                    <item.screenComponent
+                      nextRoute={this.getNextRoute(SURVEY_LOGIN, i)}
+                      onSubmit={(data) => {
+                        this.logAnswerLogin(item.questionId, data);
+                      }}
+                      data={this.state.surveyAnswersLogin[item.questionId]}
+                      globalData={{
+                        nickname: this.getValueOrNone(
+                          this.state.surveyAnswersLogin,
+                          "anmeldung",
+                          "nickname"
+                        ),
+                        numberOfVisits:
+                          this.state.surveyAnswersLogin[
+                            "calculatedNumberOfVisits"
+                          ],
+                      }}
+                      onLogin={(userId) => {
+                        this.logAnswerLogin("userID", userId);
+                        this.saveNumberOfVisits(userId);
+                      }}
+                      onFinalSubmit={() => this.onFinalSubmitLogin()}
+                      firebaseClient={this.firebaseClient}
+                      onError={(errorCode) => this.reportError(errorCode)}
+                    ></item.screenComponent>
+                  }
+                />
+              ))}
 
-            {SURVEY_REGISTRIERUNG.surveyItems.map((item, i) => (
-              <Route
-                key={item.questionId}
-                path={`${SURVEY_REGISTRIERUNG.baseUrl}/${item.questionId}`}
-                element={
-                  <item.screenComponent
-                    nextRoute={this.getNextRoute(SURVEY_REGISTRIERUNG, i)}
-                    onSubmit={(data) => {
-                      this.logAnswerRegistrierung(item.questionId, data);
-                    }}
-                    data={
-                      this.state.surveyAnswersRegistrierung[item.questionId]
-                    }
-                    globalData={{
-                      nickname:
-                        this.state.surveyAnswersRegistrierung["nickname"],
-                    }}
-                    onNicknameSubmit={() => this.createNewUser()}
-                    onFinalSubmit={
-                      () => this.onFinalSubmitRegistrierung()
-                    }
-                    onLogout={() => this.resetSurveyData()}
+              {SURVEY_LOGIN_DRITTERBESUCH.surveyItems.map((item, i) => (
+                <Route
+                  key={item.questionId}
+                  path={`${SURVEY_LOGIN_DRITTERBESUCH.baseUrl}/${item.questionId}`}
+                  element={
+                    <item.screenComponent
+                      nextRoute={this.getNextRoute(
+                        SURVEY_LOGIN_DRITTERBESUCH,
+                        i
+                      )}
+                      onSubmit={(data) => {
+                        this.logAnswerLogin(item.questionId, data);
+                      }}
+                      data={this.state.surveyAnswersLogin[item.questionId]}
+                      globalData={{
+                        nickname: this.getValueOrNone(
+                          this.state.surveyAnswersLogin,
+                          "anmeldung",
+                          "nickname"
+                        ),
+                      }}
+                      onFinalSubmit={() => this.onFinalSubmitLogin()}
+                    ></item.screenComponent>
+                  }
+                />
+              ))}
 
-                  ></item.screenComponent>
-                }
-              />
-            ))}
-
-            {SURVEY_LOGIN.surveyItems.map((item, i) => (
-              <Route
-                key={item.questionId}
-                path={`${SURVEY_LOGIN.baseUrl}/${item.questionId}`}
-                element={
-                  <item.screenComponent
-                    nextRoute={this.getNextRoute(SURVEY_LOGIN, i)}
-                    onSubmit={(data) => {
-                      this.logAnswerLogin(item.questionId, data);
-                    }}
-                    data={this.state.surveyAnswersLogin[item.questionId]}
-                    globalData={{
-                      nickname: this.getValueOrNone(this.state.surveyAnswersLogin, "anmeldung", "nickname"),
-                      numberOfVisits: this.state.surveyAnswersLogin['calculatedNumberOfVisits']
-                    }}
-                    onLogin={(userId) => { this.logAnswerLogin('userID', userId); this.saveNumberOfVisits(userId); }}
-                    onFinalSubmit={
-                      () => this.onFinalSubmitLogin()
-                    }
-                    firebaseClient={this.firebaseClient}
-                    onError={(errorCode) => this.reportError(errorCode)}
-                  ></item.screenComponent>
-                }
-              />
-            ))}
-
-            {SURVEY_LOGIN_DRITTERBESUCH.surveyItems.map((item, i) => (
-              <Route
-                key={item.questionId}
-                path={`${SURVEY_LOGIN_DRITTERBESUCH.baseUrl}/${item.questionId}`}
-                element={
-                  <item.screenComponent
-                    nextRoute={this.getNextRoute(SURVEY_LOGIN_DRITTERBESUCH, i)}
-                    onSubmit={(data) => {
-                      this.logAnswerLogin(item.questionId, data);
-                    }}
-                    data={this.state.surveyAnswersLogin[item.questionId]}
-                    globalData={{
-                      nickname: this.getValueOrNone(this.state.surveyAnswersLogin, "anmeldung", "nickname")
-                    }}
-                    onFinalSubmit={
-                      () => this.onFinalSubmitLogin()
-                    }
-                  ></item.screenComponent>
-                }
-              />
-            ))}
-
-            {SURVEY_GUEST.surveyItems.map((item, i) => (
-              <Route
-                key={item.questionId}
-                path={`${SURVEY_GUEST.baseUrl}/${item.questionId}`}
-                element={
-                  <item.screenComponent
-                    nextRoute={this.getNextRoute(SURVEY_GUEST, i)}
-                    onSubmit={(data) => {
-                      this.logAnswerGuest(item.questionId, data);
-                    }}
-                    data={this.state.surveyAnswersGuest[item.questionId]}
-                    onFinalSubmit={
-                      () => this.onFinalSubmitGuest()
-                    }
-                  ></item.screenComponent>
-                }
-              />
-            ))}
-          </Route>
-        </Routes>
-      </BrowserRouter></>
+              {SURVEY_GUEST.surveyItems.map((item, i) => (
+                <Route
+                  key={item.questionId}
+                  path={`${SURVEY_GUEST.baseUrl}/${item.questionId}`}
+                  element={
+                    <item.screenComponent
+                      nextRoute={this.getNextRoute(SURVEY_GUEST, i)}
+                      onSubmit={(data) => {
+                        this.logAnswerGuest(item.questionId, data);
+                      }}
+                      data={this.state.surveyAnswersGuest[item.questionId]}
+                      onFinalSubmit={() => this.onFinalSubmitGuest()}
+                    ></item.screenComponent>
+                  }
+                />
+              ))}
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </>
     );
   }
 }
