13500c775253f3c71fde5ea4dc487710da24d6a0
letnorx <147499884+letnorx@users.noreply.github.com>
2023-10-17 15:38:20 +0200

    remove free value from gender questions

--- src/screens/Registrierung/05_Geschlecht.js
+++ src/screens/Registrierung/05_Geschlecht.js
@@ -1,24 +1,16 @@
 import WeiterButton from "../../components/WeiterButton";
 import MultipleChoiceTool from "../../components/MultipleChoiceTool";
-import TextInput from "../../components/TextInput";
 import VerticalGrid from "../../components/VerticalGrid";
 
-const ANSWER_OPTIONS = ["männlich", "weiblich", "divers", "keine Angabe"];
+const ANSWER_OPTIONS = ["divers", "weiblich", "männlich", "keine Angabe"];
 
 function changePredefinedValues(data, vals) {
   return { ...data, predefinedValues: vals };
 }
 
-function changeFreeValue(data, val) {
-  return { ...data, freeValue: val };
-}
-
 function Screen({ onSubmit, data, nextRoute }) {
   function hasUserAnswered() {
     if (data && data.predefinedValues && data.predefinedValues.length > 0) {
-      return true;
-    }
-    if (data && data.freeValue && data.freeValue.length > 0) {
       return true;
     }
     return false;
@@ -33,16 +25,12 @@ function Screen({ onSubmit, data, nextRoute }) {
           onChange={(vals) => onSubmit(changePredefinedValues(data, vals))}
           data={(data && data.predefinedValues) || null}
         />
-        <TextInput
-          value={(data && data.freeValue) || ""}
-          onChange={(val) => onSubmit(changeFreeValue(data, val))}
+        <WeiterButton
+          disabled={!hasUserAnswered()}
+          style={{ position: "relative", right: "auto", bottom: "auto" }}
+          navigateTo={nextRoute}
         />
-        <WeiterButton disabled={!hasUserAnswered()} style={{ position: "relative", right: "auto", bottom: "auto" }} navigateTo={nextRoute} />
-
       </VerticalGrid>
-
-
-
     </>
   );
 }
