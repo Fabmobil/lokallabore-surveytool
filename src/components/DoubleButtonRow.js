bd1c30adedf3f61bcad62505adfb6b6012f2c34f
letnorx <147499884+letnorx@users.noreply.github.com>
2023-10-18 16:08:06 +0200

    fix layout of DoubleButtonRow

--- src/components/DoubleButtonRow.js
+++ src/components/DoubleButtonRow.js
@@ -1,6 +1,10 @@
 function DoubleButtonRow({ children }) {
   return (
-    <div className={"flex fixed bottom-4 md-h:bottom-14 right-6 left-6"}>
+    <div
+      className={
+        "flex fixed bottom-4 md-h:bottom-14 right-6 left-6 justify-between"
+      }
+    >
       {children}
     </div>
   );
